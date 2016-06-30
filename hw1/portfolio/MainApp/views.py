c9bf14bc19cdb6b4429eb40ee19e49399694b23a
jsay-api <julia.sayapina@me.com>
2016-06-30 12:58:33 +0300

    base.html template added, inc-menu.html added, index1.html and port1.html became templated with {%%}. contacts.html modified

--- hw1/portfolio/MainApp/views.py
+++ hw1/portfolio/MainApp/views.py
@@ -3,10 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
 	#name = 'Julia'
-	return render(request, 'index.html')
+	return render(request, 'index1.html')
 
 def port(request):
-	return render(request, 'port.html')
+	return render(request, 'port1.html')
 
 def eduw(request):
 	name = 'Julia'
