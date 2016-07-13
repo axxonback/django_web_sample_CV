fa9f4535fa9f44ce236ca2ee937111c4513317c4
jsay-api <julia.sayapina@me.com>
2016-07-13 19:05:11 +0300

    feature added: on /edu and /exp when the pin img clicked google maps preview is toggled in a dropdown

--- hw1/portfolio/MainApp/models.py
+++ hw1/portfolio/MainApp/models.py
@@ -1,21 +1,24 @@
 from django.db import models
+from django_date_extensions.fields import PrettyDateField, ApproximateDateFormField
 
 class Employer(models.Model):
 	employer_name = models.CharField(verbose_name = "Name", max_length = 50)
 	employer_site = models.URLField(verbose_name = "Site", max_length = 64, blank = True)
-	employer_location = models.CharField(verbose_name = "Location", max_length = 300)
+	employer_location = models.URLField(verbose_name = "Location", max_length = 300)
 
 class StudyPlace(models.Model):
 	stp_name = models.CharField(verbose_name = "Place of study", max_length = 80)
 	stp_site = models.URLField(verbose_name = "Site", max_length = 64, blank = True)
-	stp_location = models.CharField(verbose_name = "Location", max_length = 300)
+	stp_location = models.URLField(verbose_name = "Location", max_length = 300)
 		
 
 class Experience(models.Model):
 	period = models.CharField(verbose_name = "Period of work", max_length = 50)
+	start_date = models.DateField(verbose_name = 'Date of start', blank = True)
 	org = models.ForeignKey('Employer', verbose_name = "Employer")
 	position = models.CharField(verbose_name = "Position", max_length = 30)
 	duties = models.TextField(verbose_name = "Duties")
+	classname = models.CharField(verbose_name = 'Class name', blank=True, max_length = 100)
 	
 
 class Education(models.Model):
