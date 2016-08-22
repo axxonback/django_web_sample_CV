c870053a3c8e65ea18e469696ee77219c4c53c1c
jsay-api <julia.sayapina@me.com>
2016-08-22 12:09:13 +0300

    additional files and changes for Heroku

--- hw1/portfolio/portfolio/settings.py
+++ hw1/portfolio/portfolio/settings.py
@@ -133,3 +133,40 @@ STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static",)
 # MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
 
 
+import dj_database_url
+
+# Update database configuration with $DATABASE_URL.
+# import dj_database_url
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
+
+DATABASES['default'] = dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
+
+DEBUG = False
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
