6e2e42c321a9ef8d9f60232ea85ddab096da3644
jsay-api <julia.sayapina@me.com>
2016-08-24 19:34:56 +0300

    production.py mod

--- portfolio/settings/production.py
+++ portfolio/settings/production.py
@@ -9,11 +9,6 @@ from urllib.parse import urlparse, uses_netloc
 DEBUG = False
 TEMPLATE_DEBUG = False
 
-# DATABASES = settings.DATABASES
-
-# Update database configuration with $DATABASE_URL.
-db_from_env = dj_database_url.config(conn_max_age=500)
-# DATABASES['default'].update(db_from_env)
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
@@ -27,21 +22,13 @@ ALLOWED_HOSTS = ['*']
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
+# DATABASES = settings.DATABASES
 
-# Simplified static file serving.
-# https://warehouse.python.org/project/whitenoise/
+# Update database configuration with $DATABASE_URL.
+db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
-# Register database schemes in URLs.
-uses_netloc.append('mysql')
 
 try:
 
@@ -71,3 +58,19 @@ try:
             DATABASES['default']['ENGINE'] = 'django.db.backends.mysql'
 except Exception:
     print ('Unexpected error:', sys.exc_info())
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+# Register database schemes in URLs.
+uses_netloc.append('mysql')
