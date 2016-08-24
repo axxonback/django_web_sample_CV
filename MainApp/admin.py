01b61d6b4b426d1c22c444f1138704ec269e3898
jsay-api <julia.sayapina@me.com>
2016-08-24 12:57:29 +0300

    admin modified

--- MainApp/admin.py
+++ MainApp/admin.py
@@ -1,3 +1,19 @@
-from django.contrib import admin
+# -*- coding: utf-8 -*-
 
-# Register your models here.
+from django.contrib import admin
+from .models import *
+
+admin.site.site_header = "Julia Sayapina | Admin"
+admin.site.site_title = "| Julia Sayapina"
+admin.site.index_title = "Administration"
+
+class EmployerAdmin(admin.ModelAdmin):
+	list_display = ['employer_name', 'employer_site', 'employer_location']
+
+
+
+admin.site.register(Employer, EmployerAdmin)
+admin.site.register(StudyPlace)
+admin.site.register(Experience)
+admin.site.register(Education)
+admin.site.register(Projects)
